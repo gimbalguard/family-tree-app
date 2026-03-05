cb923aea6bc587864e1184eff24f7e964861ca4d
Esotera <esotera.mystic@gmail.com>
2026-03-05 16:52:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1899148114-40e5e",
+  "appId": "1:1031946928906:web:47a11760ffa47dcaa0b0f3",
+  "apiKey": "AIzaSyBfMM8fV6kM0afbWPF4OLH_qT694FmJ9x0",
+  "authDomain": "studio-1899148114-40e5e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1031946928906"
+};
