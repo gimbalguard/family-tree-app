497d0e6558dfc4e465fc5c7ec7dd3d79b5b22e69
Esotera <esotera.mystic@gmail.com>
2026-03-06 20:53:03 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-1899148114-40e5e",
   "appId": "1:1031946928906:web:47a11760ffa47dcaa0b0f3",
-  "storageBucket": "studio-1899148114-40e5e.appspot.com",
+  "storageBucket": "studio-1899148114-40e5e.firebasestorage.app",
   "apiKey": "AIzaSyBfMM8fV6kM0afbWPF4OLH_qT694FmJ9x0",
   "authDomain": "studio-1899148114-40e5e.firebaseapp.com",
   "measurementId": "",
